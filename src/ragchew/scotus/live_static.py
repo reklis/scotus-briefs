7aafaed0ad1ae2f6ec4eb2db725814709bf22184
sfusco <slf@stevenfusco.com>
2026-09-06 17:11:07 -0400

    Distinguish generic legal labels from party names

--- src/ragchew/scotus/live_static.py
+++ src/ragchew/scotus/live_static.py
@@ -172,7 +172,7 @@ from ragchew.storage import ObjectMetadata, ObjectStore
 
 LOG = logging.getLogger("ragchew.scotus.live_static")
 
-POLICY_VERSION = "scotus-brief-policy-v21"
+POLICY_VERSION = "scotus-brief-policy-v22"
 DOCUMENT_TEXT_VERSION = "official-document-text-v3"
 
 
